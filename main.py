126b388e748d502bae8f9122d2a0a28e4347ac4a
anthonphax <51837231+anthonphax@users.noreply.github.com>
2024-05-17 20:07:44 -0300

    products

--- main.py
+++ main.py
@@ -8,3 +8,27 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get('/get-item/{item_id}')
+def get_item(
+    item_id: int = Path(
+        None,
+        description="Fill with ID of the item you want to view")):
+
+    search = list(filter(lambda x: x["id"] == item_id, menu))
+
+    if search == []:
+        return {'Error': 'Item does not exist'}
+
+    return {'Item': search[0]}
+
+
+@app.get('/get-by-name')
+def get_item(name: Optional[str] = None):
+
+    search = list(filter(lambda x: x["name"] == name, menu))
+
+    if search == []:
+        return {'item': 'Does not exist'}
+
+    return {'Item': search[0]}
