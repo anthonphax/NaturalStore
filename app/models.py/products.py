71c623f457bb56041799206248538b4b1d9be0e3
anthonphax <51837231+anthonphax@users.noreply.github.com>
2024-05-17 20:21:11 -0300

    products routes

--- app/models.py/products.py
+++ app/models.py/products.py
@@ -1,3 +1,23 @@
+class Product:
+    def __init__(self, name, description, cost, price, amount, purchased, sold, weight, manufacturer, group, subgroup=None ):
+        self.name = name
+        self.description = description
+        self.cost = cost
+        self.price = price
+        self.amount = amount
+        self.purchased = purchased
+        self.sold = sold
+        self.weight = weight
+        self.manufacturer = manufacturer
+        self.group = group
+        self.subgroup = subgroup
+
+
+class ProductFactory:
+    @staticmethod
+    def create_Product(Product_type: str) -> Product:
+        pass
+
 products = [
     {   'id': 1,
         'name': 'coffee',
--- app/routers/products.py
+++ app/routers/products.py
@@ -1,27 +1,7 @@
 from fastapi import FastAPI, HTTPException
+import models.products
 
 app = FastAPI()
-
-class Product:
-    def __init__(self, name, description, cost, price, amount, purchased, sold, weight, manufacturer, group, subgroup=None ):
-        self.name = name
-        self.description = description
-        self.cost = cost
-        self.price = price
-        self.amount = amount
-        self.purchased = purchased
-        self.sold = sold
-        self.weight = weight
-        self.manufacturer = manufacturer
-        self.group = group
-        self.subgroup = subgroup
-
-
-class ProductFactory:
-    @staticmethod
-    def create_Product(Product_type: str) -> Product:
-        pass
-
 
 @app.get('/products')
 def list_products():    
